ff2db12b83d0802be8ba0ed866339de450467a8b
Katherine Beame <katherine.beame@gmail.com>
2016-04-13 10:15:44 -0700

    got rid of ''

--- kat/gulpfile.js
+++ kat/gulpfile.js
@@ -4,7 +4,7 @@ const eslint = require('gulp-eslint');
 var files = ['./test/**/*test.js', 'gulpfile.js', 'server.js'];
 
 gulp.task('lint', () => {
-  return gulp.src('files')
+  return gulp.src(files)
   .pipe(eslint({
     'env': [
       'mocha'
