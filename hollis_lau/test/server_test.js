2cf2932c02bb89ee57ad3eec3dc0f6d9a018ecc7
Hollis Lau <hollislau@hotmail.com>
2016-04-14 10:51:40 -0700

    Add basic routing and tests for routes

---
+++ hollis_lau/test/server_test.js
@@ -0,0 +1,53 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request;
+require(__dirname + "/../server");
+
+describe("server", () => {
+  it("should respond 404 on bad requests", (done) => {
+    request("localhost:3000")
+    .get("/badroute")
+    .end((err, res) => {
+      expect(err).to.eql(err);
+      expect(res).to.have.status(404);
+      expect(res.text).to.eql("Not found, bitches!");
+      done();
+    });
+  });
+
+  it("should respond to /time with the current server time", (done) => {
+    request("localhost:3000")
+    .get("/time")
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql(res.header.date);
+      done();
+    });
+  });
+
+  it("should respond to /greet/[name] by greeting [name]", (done) => {
+    request("localhost:3000")
+    .get("/greet/Bob")
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql("Hello Bob!");
+      done();
+    });
+  });
+
+  it("should accept POST requests to /greet", (done) => {
+    request("localhost:3000")
+    .post("/greet")
+    .send({ "name": "Bob" })
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql("Hello Bob!");
+      done();
+    });
+  });
+});
