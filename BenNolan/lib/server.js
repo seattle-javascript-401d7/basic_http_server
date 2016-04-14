6f030fd94f0ac34896d37be6edef43d5119de41d
bonecrushereb <bonecrushereb@gmail.com>
2016-04-14 08:53:17 -0700

    /time is working, working on /greet/user

---
+++ BenNolan/lib/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const server = module.exports = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/time') {
+    var date = new Date();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('' + date);
+    return res.end();
+  }
+
+  if (req.method === 'GET' && req.url === '/greet') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Greetings');
+    return res.end();
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/html' });
+  res.write('<h1>Error 404</h1>');
+  return res.end();
+});
+
+server.listen(3000, () => process.stdout.write('server up on 3000'));
---
+++ BenNolan/test/http_server_test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request;
+require(__dirname + '/../lib/server');
+
+describe('Server is running', () => {
+  it('should accept GET requests to /time', (done) => {
+    var date = new Date();
+    request('localhost:3000')
+    .get('/time')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql('' + date);
+      done();
+    });
+  });
+
+  it('should 404 on bad requests', (done) => {
+    request('localhost:3000')
+    .get('/badroute')
+    .end((err, res) => {
+      expect(res.status).to.eql(404);
+      expect(res.text).to.eql('<h1>Error 404</h1>');
+      done();
+    });
+  });
+
+  it('should accept GET requests to /greet', (done) => {
+    request('localhost:3000')
+    .get('/greet')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql('Greetings');
+      done();
+    });
+  });
+});
