c321f0be610048c20308e0f67b8275a8950bbcf1
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-13 21:41:42 -0700

    add superagent request to index page to post data to server and console.log a response

--- carrie-short/index.js
+++ carrie-short/index.js
@@ -1,1 +1,13 @@
 require(__dirname + '/lib/server');
+var request = require('superagent');
+var name = process.argv[2] || 'Oh No Mr Bill';
+request
+  .post('http://localhost:3000/greet')
+  .send({ 'name': name })
+  .end((err, res) => {
+    if (err || !res.ok) {
+      console.log('Oh no! error');
+    } else {
+      console.log(res.text);
+    }
+  });
