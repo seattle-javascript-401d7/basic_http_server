0f26a074b474efe53bc0909f04224e922b9e8280
Katherine Beame <katherine.beame@gmail.com>
2016-04-14 08:57:56 -0700

    mocha is having a problem with request

--- kat/test/server_test.js
+++ kat/test/server_test.js
@@ -6,27 +6,38 @@ require(__dirname + '/../server.js');
 chai.use(chaiHttp);
 
 describe('The HTTP Server', () => {
-it('should accept a GET request to /time', (done) => {
-  request('localhost:3000')
-  .get('/time')
-  .end((err, response) => {
-    expect(err).to.eql(null);
-    expect(response.status).to.eql(200);
-    expect(response.text).to.eql(Date().toString());
-    done();
-    });
-  });
-  it('should accept a GET request to /greet/name', (done) => {
-    var name = process.argv[2] = 'Kat';
+  var name = process.argv[2] = 'Kat';
+  it('should accept a GET request to /time', (done) => {
     request('localhost:3000')
     .get('/time')
     .end((err, response) => {
       expect(err).to.eql(null);
       expect(response.status).to.eql(200);
-      expect(response.text).to.eql(name);
+      expect(response.text).to.eql(Date().toString());
       done();
     });
   });
+  it('should accept a GET request to /greet/name', (done) => {
+    request('localhost:3000')
+    .get('/greet/name')
+    .end((err, response) => {
+      expect(err).to.eql(null);
+      expect(response.status).to.eql(200);
+      expect(response.text).to.eql(name);
+      console.log(name);
+      done();
+    });
+  });
+  it('should accept POST requests to /greet', (done) => {
+    request('localhost:3000')
+    .post('/greet')
+    .send({'name': process.argv[2].toString()})
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(200);
+      expect(res.text).to.eql(process.argv[2]);
+      done();
+    });
 it('should be 418 on a bad request', (done) => {
   request('localhost:3000')
   .get('/crackpot')
