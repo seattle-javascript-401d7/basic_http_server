e694ce8d7552a282ca2e611f4c3674f381b17214
BRANDON PARKER <Brandon@dkparker.com>
2016-04-14 10:35:54 -0700

    added gulpfile and cleaned up code

--- test/test.js
+++ test/test.js
@@ -12,30 +12,30 @@ describe('the HTTP server response', () => {
       .end((err, res) => {
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
-        // expect(res).to.have.status(200);
         expect(res.text).to.eql('Greetings newUser');
-        done();
-      });
-  });
-
-  it('should 404 on bad requests', (done) => {
-    request('localhost:3000')
-      .get('/badroute')
-      .end((err, res) => {
-        expect(res.status).to.eql(404);
-        expect(res.text).to.eql('this is a 404 message, yo');
         done();
       });
   });
 
   it('should accept POST request to /greet/newUser takes it as JSON', (done) => {
     request('localhost:3000')
-      .post('/greet/newUser')
-      .send({ 'greeting': 'Greetings ' })
+    .post('/greet/newUser')
+    .send({ 'greeting': 'Greetings ' })
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql('Greetings newUser');
+      done();
+    });
+  });
+
+  it('should 404 on bad requests', (done) => {
+    request('localhost:3000')
+      .get('/badroute')
       .end((err, res) => {
-        expect(err).to.eql(null);
-        expect(res).to.have.status(200);
-        expect(res.text).to.eql('Greetings newUser');
+        expect(err).to.not.eql(null);
+        expect(res.status).to.eql(404);
+        expect(res.text).to.eql('this is a 404 message, yo');
         done();
       });
   });
